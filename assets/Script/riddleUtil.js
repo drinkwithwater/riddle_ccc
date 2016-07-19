d3572b6afa94e09d59bf935ef1888e6ab66f361a
cz <598799959@qq.com>
2016-07-19 17:15:43 +0800

    add class

--- assets/Script/riddleUtil.js
+++ assets/Script/riddleUtil.js
@@ -102,6 +102,55 @@ var riddleUtil={
             var unit=this.unit;
             return bm.unitCreateBullet(unit,1);
         }
-    }
+    },
 }
+
+(function(riddleUtil){
+    var Class=riddleUtil.Class=function(){
+        this._id=_.uniqueId();
+    }
+    this.__super__=false;
+    Class.prototype={
+        _super:function(){
+            if(this.__super__){
+                this.__super__.constructor.apply(this,arguments);
+            }
+        }
+    }
+    //copy from backbone.js
+    var classExtend = function(protoProps, staticProps) {
+        var parent = this;
+        var cls;
+
+        // The constructor function for the new subclass is either defined by you
+        // (the "constructor" property in your `extend` definition), or defaulted
+        // by us to simply call the parent's constructor.
+        if (protoProps && _.has(protoProps, 'constructor')) {
+            cls = protoProps.constructor;
+        } else {
+            cls = function(){return parent.apply(this, arguments);}
+        }
+
+        // Add static properties to the constructor function, if supplied.
+        _.extend(cls, parent, staticProps);
+
+        // Set the prototype chain to inherit from `parent`, without calling
+        // `parent`'s constructor function.
+        var ClsProto = function(){ this.constructor = cls; };
+        ClsProto.prototype = parent.prototype;
+        cls.prototype = new ClsProto;
+
+        // Add prototype properties (instance properties) to the subclass,
+        // if supplied.
+        if (protoProps) _.extend(cls.prototype, protoProps);
+
+        // Set a convenience property in case the parent's prototype is needed
+        // later.
+        cls.__super__ = parent.prototype;
+
+        return cls;
+    };
+    Class.extend=classExtend;
+})(riddleUtil)
+
 module.exports=riddleUtil;
--- assets/Script/skill/Skill.js
+++ assets/Script/skill/Skill.js
@@ -1,20 +1,57 @@
 var SkillType={
+    NONE:0,
     HIT:1,
     MOVE:2,
     STAND:3,
     HURT:4,
 }
-var SkillObject=function(type){
-    this.type=type;
+var SkillBase=riddleUtil.Class.extend({
+    type:SkillType.NONE,
+    constructor:function(){
+        this._super();
+    },
+    update:function(dt){
+    },
+});
+var hitState={
+    BEFORE:1,
+    AFTER:2,
+    IDLE:3,
 }
-var HitSkill=function(){
-    this.type=SkillType.HIT;
-    this.beforeCoolTime=1;
-    this.afterCoolTime=1;
-    this.update=function(dt){
-    }
-}
+var HitSkill=SkillBase.extend({
+    type:SkillType.HIT,
+    unitBase:null,
+    beforeCoolTime:0,
+    afterCoolTime:0,
+    state:hitState.BEFORE,
+    constructor:function(){
+        this._super();
+    },
+    update:function(dt){
+        switch(this.state){
+            case hitState.BEFORE:
+                this.beforeCoolTime--;
+                if(this.beforeCoolTime==0){
+                    this.hitFunc();
+                    this.state=hitState.AFTER;
+                }
+                break;
+            case hitState.AFTER:
+                this.afterCoolTime--;
+                if(this.afterCoolTime==0){
+                    this.state=hitState.IDLE;
+                }
+                break;
+            case hitState.IDLE:
+                break;
+            default:
+                break;
+        }
+    },
+    hitFunc=function(){
+    },
+});
 module.exports={
     SkillType:SkillType,
-    SkillObject:SkillObject
+    SkillBase:SkillBase
 }
